a2308827db835eb6ae4f16ab5ae85259baff0393
kimtnguyen1 <ktn1130@gmail.com>
2022-05-25 08:16:26 -0700

    Start game button

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -56,9 +56,17 @@ function makeInvitedButton(socket_id) {
     return newNode;
 }
 
-function makePlayButton() {
+function makePlayButton(socket_id) {
     let newHTML = "<button type='button' class='btn btn-success'>Play</button>";
     let newNode = $(newHTML);
+    newNode.click(() => { 
+        let payload = {
+            requested_user: socket_id
+        }
+        console.log('**** Client log message, sending \'game_start\' command: ' + JSON.stringify(payload));
+        socket.emit('game_start', payload);
+    }
+    );
     return newNode;
 }
 
@@ -90,8 +98,8 @@ socket.on('invited', (payload) => {
         console.log(payload.message);
         return;
     }
-    let newNode = makePlayButton();
-    $('.socket_' + payload.socket_id + 'button').replaceWith(newNode);    
+    let newNode = makePlayButton(payload.socket_id);
+    $('.socket_' + payload.socket_id + ' button').replaceWith(newNode);    
 })
 
 socket.on('uninvited', (payload) => {
@@ -104,8 +112,23 @@ socket.on('uninvited', (payload) => {
         return;
     }
     let newNode = makeInviteButton(payload.socket_id);
-    $('.socket_' + payload.socket_id + 'button').replaceWith(newNode);    
+    $('.socket_' + payload.socket_id + ' button').replaceWith(newNode);    
 })
+
+socket.on('game_start_response', (payload) => {
+    if ((typeof payload == 'undefined') || (payload === null)) {
+        console.log('Server did not send a payload');
+        return;
+    }
+    if (payload.result === 'fail') {
+        console.log(payload.message);
+        return;
+    }
+    let newNode = makeStartGameButton();
+    $('.socket_' + payload.socket_id + ' button').replaceWith(newNode);    
+    /* Jump to the game page */
+    window.location.href = 'game.html?username=' + username + '&game_id=' + payload.game_id;
+}); 
 
 socket.on('join_room_response', (payload) => {
     if ((typeof payload == 'undefined') || (payload === null)) {
